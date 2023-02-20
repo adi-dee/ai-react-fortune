370772d52cc17722212c0a1432bea2bd72575e71
adi-dee <didiaviram@gmail.com>
2023-02-20 17:56:54 -0500

    css changes

--- src/components/FortuneTeller.js
+++ src/components/FortuneTeller.js
@@ -3,7 +3,7 @@ import React from "react";
 const FortuneTeller = ({ fortune, auraColor, generateFortune }) => {
   return (
     <div className="result">
-      <h2 className="fortune-h2">Your personalised Fortune</h2>
+      <h2 className="fortune-h2">Your Personalised Fortune</h2>
     {!fortune ? 
     <div className="effect-1" onClick={generateFortune}>
       <span>Read your fortune</span>
